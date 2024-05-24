6965cf7a0d20c32e360f51d057189325a091a187
Zunaed Sazzad Tonay <107871196+zunaedsazzad@users.noreply.github.com>
2024-05-25 04:52:30 +0600

    abc

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDaAdE_SXol-wSKHWz4a7yFVRCZqURpFVg",
-    authDomain: "chatbox-25e84.firebaseapp.com",
-    projectId: "chatbox-25e84",
-    storageBucket: "chatbox-25e84.appspot.com",
-    messagingSenderId: "602668839748",
-    appId: "1:602668839748:web:fac406ef97431326cb9d11",
-    measurementId: "G-G5Y92ZX8MH"
+  apiKey: "AIzaSyDaAdE_SXol-wSKHWz4a7yFVRCZqURpFVg",
+  authDomain: "chatbox-25e84.firebaseapp.com",
+  databaseURL: "https://chatbox-25e84-default-rtdb.firebaseio.com",
+  projectId: "chatbox-25e84",
+  storageBucket: "chatbox-25e84.appspot.com",
+  messagingSenderId: "602668839748",
+  appId: "1:602668839748:web:9972f03ac33e0475cb9d11",
+  measurementId: "G-LW8RR9BRQW"
   };
   const app = initializeApp(firebaseConfig);
   export const auth = getAuth(app);
